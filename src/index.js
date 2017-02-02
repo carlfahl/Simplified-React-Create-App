60c3a8bbbc08b4621414414e85c88c69e7cf9b37
Carl A. Fahlstrom <carl.fahlstrom@gmail.com>
2017-02-02 15:06:08 -0700

    added react-bootstrap to dependencies and implimented hello world test and working on table of todos.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import {Jumbotron} from 'react-bootstrap';
 
 var HelloWorld = React.createClass({
   render: function() {
     return (
-      <div>
-        <h1>Hello, world!</h1>
-      </div>
-    )
+      <Jumbotron>
+        <h1>Hello World!!!</h1>
+        <h2>It is good to be in the World</h2>
+        <p>I hope it is for you as well!!</p>
+      </Jumbotron>
+    );
   }
-})
+});
+
+var TableListing = React.createClass({
+  setInitialState: function () {
+    return {
+      todos: [{name: 'Write todo app in React', date: '2/2/17', status: 'ToDo'},
+              {name: 'Make Bears CRUD Api', date:'2/2/17', status:'Done'},
+              {name: 'Make the Bears App Reactified', date: '2/2/17', status:'Done'}],
+    };
+  },
+  render: function () {
+    var trows = this.state.todos.reduce(function (preVal, item) {
+      return ('<tr><td>' + item.name + '</td><td>' + item.date + '</td><td>' + item.status + '</td><td> Complete </td></tr>');
+    }, '');
+    return (
+      <div>
+        <table>
+          <thead>
+            <th><strong>Name</strong></th>
+            <th><strong>Date</strong></th>
+            <th><strong>Status</strong></th>
+            <th><strong>Action</strong></th>
+          </thead>
+          <tbody>
+            {trows}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+});
+
+var MyApp = React.createClass({
+  render: function () {
+    return (
+      <div>
+        <HelloWorld />
+        <TableListing />
+      </div>
+    );
+  }
+});
 
 ReactDOM.render(
-  <HelloWorld />,
+  <MyApp />,
   document.getElementById('root')
 );
